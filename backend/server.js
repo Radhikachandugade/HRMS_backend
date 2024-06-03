c805897c24b36ddd1192c3e273fd8975f5d85cb4
Radhika_Chandugade <chandugaderadhika@gmail.com>
2024-06-03 15:51:27 +0530

    made some changes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import express from "express";
 import path from "path";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import attendanceRoutes from "./routes/attendenceRoutes.js";
 import leaveRequest from "./routes/leaveRoutes.js";
@@ -33,23 +32,11 @@ app.get("/test-cors", cors(corsOptions), (req, res) => {
 
 app.use("/api/users", userRoutes);
 app.use("/api/attendance", attendanceRoutes);
-app.use("/api/uploads", uploadRoutes);
 app.use("/api/leaves", leaveRequest);
-// Create a static folder
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// } else {
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-// }
 
 app.use(notFound);
 app.use(errorHandler);
